e4233e06a89e30bbb2489d230706ab4563b61dc9
Janson Kang <temasek71@gmail.com>
2015-12-30 02:34:06 +0800

    Add QS Tiles
    * music
    * reboot/recovery
    
    modified for hydrothunder as only fixing addition of couple tiles
    
    Signed-off-by: Janson Kang <temasek71@gmail.com>
    Signed-off-by: Joshndroid <fmxjoshiewah@gmail.com>

--- sdk/src/java/org/cyanogenmod/internal/util/QSConstants.java
+++ sdk/src/java/org/cyanogenmod/internal/util/QSConstants.java
@@ -56,6 +56,8 @@ public class QSConstants {
     public static final String TILE_EDIT = "edit";
     public static final String TILE_DND = "dnd";
     public static final String TILE_RESTARTUI = "restartui";
+    public static final String TILE_MUSIC = "music";
+    public static final String TILE_REBOOT = "reboot";
 
     public static final String DYNAMIC_TILE_NEXT_ALARM = "next_alarm";
     public static final String DYNAMIC_TILE_IME_SELECTOR = "ime_selector";
@@ -104,6 +106,8 @@ public class QSConstants {
         STATIC_TILES_AVAILABLE.add(TILE_CAFFEINE);
 	STATIC_TILES_AVAILABLE.add(TILE_PIE);
         STATIC_TILES_AVAILABLE.add(TILE_RESTARTUI);
+        STATIC_TILES_AVAILABLE.add(TILE_MUSIC);
+        STATIC_TILES_AVAILABLE.add(TILE_REBOOT);
 
         TILES_AVAILABLE.addAll(STATIC_TILES_AVAILABLE);
 
